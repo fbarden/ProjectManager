d0e3642f9b5b69aa879cd379dabfce2ef916cf41
Felipe Barden <felipebarden@gmail.com>
2013-06-01 14:11:02 -0300

    Add main file.

---
+++ projectManager.py
@@ -0,0 +1,13 @@
+from views.MainWindow import MainWindow
+import os, sys
+from PyQt4.QtGui import QApplication
+
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir) 
+
+globalProject = None
+
+app = QApplication(sys.argv)
+f = MainWindow()
+f.show()
+app.exec_()
