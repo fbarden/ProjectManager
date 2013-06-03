ab7dd796e180c86d49663120f92b352d1d6d791f
Felipe Barden <felipebarden@gmail.com>
2013-06-03 16:50:44 -0700

    WIP: Create clickable document links.

--- views/DocumentViewWidget.py
+++ views/DocumentViewWidget.py
@@ -23,27 +23,38 @@ class DocumentViewWidget(QWidget):
         self.document = document
         cursor = self.ui.textBrowser.textCursor()
         self.loadTitle()
-        cursor.insertBlock()
         self.loadClauses()
     
     def loadTitle(self):
         cursor = self.ui.textBrowser.textCursor()
-        cursor.insertBlock()
         cursor.insertText(self.document.getTitle())
 
     def loadClauses(self):
         clausesList = self.document.getClausesList();
         for clause in clausesList :
-            self.loadClause(clause)
             cursor = self.ui.textBrowser.textCursor()
-            cursor.insertBlock()
             cursor.movePosition(QTextCursor.End)
+            cursor.insertBlock()
+            cursor.insertBlock()
+            self.loadClause(clause)
 
-    def loadClause(self,  clause):
+    def loadClause(self,  clauseID):
+        clause = self.document.getClause(clauseID)
         cursor = self.ui.textBrowser.textCursor()
         cursor.movePosition(QTextCursor.End)
         table = cursor.insertTable(2,  3)
         table.mergeCells(0, 0, 1, 3)
+        cursor = table.cellAt(0, 0).firstCursorPosition()
+        cursor.insertText(clause.getTitle())
         cursor = table.cellAt(1, 1).firstCursorPosition()
-        cursor.insertText(self.document.getClause(clause).getText())
-        
+        cursor.insertText(clause.getText())
+        uplinks = clause.getParentLinksDoc2Clause()
+        cursor = table.cellAt(1, 0).lastCursorPosition()
+        for linkDoc in uplinks.keys() :
+            link = '<a href="' + linkDoc + '">' + linkDoc + '</a>'
+            cursor.insertHtml(link)
+        cursor = table.cellAt(1, 2).lastCursorPosition()
+        downlinks = clause.getChildLinksDoc2Clause()
+        for linkDoc in downlinks.keys() :
+            link = '<a href="' + linkDoc + '">' + linkDoc + '</a>'
+            cursor.insertHtml(link)
