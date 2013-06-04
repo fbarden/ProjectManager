cd908da3212d75d70d3f9075f7623c274d1496c2
Felipe Barden <felipebarden@gmail.com>
2013-06-03 21:53:32 -0300

    Add tag to the links. Also add link to Clause Title.

--- views/DocumentViewWidget.py
+++ views/DocumentViewWidget.py
@@ -45,16 +45,17 @@ class DocumentViewWidget(QWidget):
         table = cursor.insertTable(2,  3)
         table.mergeCells(0, 0, 1, 3)
         cursor = table.cellAt(0, 0).firstCursorPosition()
-        cursor.insertText(clause.getTitle())
+	link = '<a href="clause:' + clause.getID() + '">' + clause.getTitle() + '</a>'
+        cursor.insertHtml(link)
         cursor = table.cellAt(1, 1).firstCursorPosition()
         cursor.insertText(clause.getText())
         uplinks = clause.getParentLinksDoc2Clause()
         cursor = table.cellAt(1, 0).lastCursorPosition()
         for linkDoc in uplinks.keys() :
-            link = '<a href="' + linkDoc + '">' + linkDoc + '</a>'
+            link = '<a href="document:' + linkDoc + '">' + linkDoc + '</a>'
             cursor.insertHtml(link)
         cursor = table.cellAt(1, 2).lastCursorPosition()
         downlinks = clause.getChildLinksDoc2Clause()
         for linkDoc in downlinks.keys() :
-            link = '<a href="' + linkDoc + '">' + linkDoc + '</a>'
+            link = '<a href="document:' + linkDoc + '">' + linkDoc + '</a>'
             cursor.insertHtml(link)
